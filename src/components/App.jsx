dd1990c885cde8aa50c65b36f9edb9606d4e64bb
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-31 22:46:09 -0700

    Usestate React Hook

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  //array destructuring (es6)
+  // const [red, green, blue] = [9, 132, 227];
+
+  // console.log(green);
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
